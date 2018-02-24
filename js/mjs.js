23c634ddd0fa56d12bfb68412a1ab28d780d2961
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2018-02-25 05:36:14 +0700

    Resposive ok! hoan thien trang chu, gioi thieu, lien he con thieu Map API

---
+++ js/mjs.js
@@ -0,0 +1,16 @@
+$ ( function () {
+   function initMap() {
+        var location = new google.maps.LatLng(21.028802,105.781693);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        
+   }
+
+   google.maps.event.addDomlistener(window,'load',initMap());
+});
