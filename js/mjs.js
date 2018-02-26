a18f4768c351765fac8438e3d2b0fbdf89c7e8d7
Jerry <16341051+jerryle@users.noreply.github.com>
2018-02-26 11:17:51 +0700

    fix conflict

--- js/mjs.js
+++ js/mjs.js
@@ -1,5 +1,4 @@
 //Google Map API
-<<<<<<< HEAD
 function initMap() {
     var options = {
         zoom: 8,
@@ -9,15 +8,6 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), options);
 };
 
-=======
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(21.028774, 105.781745), zoom: 18
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}
->>>>>>> a82767be8efb684b821f12af1f6a056b50243f8c
 // noUI slider
 var priceslid = document.getElementById('price-filter');
 var range_all_sliders = {
