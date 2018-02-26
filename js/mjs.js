a82767be8efb684b821f12af1f6a056b50243f8c
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2018-02-26 10:43:53 +0700

    chinh sua google map api

--- js/mjs.js
+++ js/mjs.js
@@ -1,20 +1,11 @@
 //Google Map API
-$ ( function () {
-   function initMap() {
-        var location = new google.maps.LatLng(21.028802,105.781693);
-
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: location,
-            zoom: 16,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        
-   }
-
-   google.maps.event.addDomlistener(window,'load',initMap());
-});
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(21.028774, 105.781745), zoom: 18
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
 // noUI slider
 var priceslid = document.getElementById('price-filter');
 var range_all_sliders = {
