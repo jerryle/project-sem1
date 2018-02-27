79873ce50efe6900de1ea2290f13e1e491111f93
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2018-02-28 04:41:51 +0700

    hoan thien web site

--- js/mjs.js
+++ js/mjs.js
@@ -1,11 +1,17 @@
 //Google Map API
 function initMap() {
-    var options = {
-        zoom: 18,
-        center:{lat:21.028774,lng:105.781745}
-    };
+    var myCenter = new google.maps.LatLng(21.028774,105.781745);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {center: myCenter, zoom: 15};
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
 
-    var map = new google.maps.Map(document.getElementById('map'), options);
+    // Zoom to 9 when clicking on marker
+    google.maps.event.addListener(marker,'click',function() {
+        map.setZoom(18);
+        map.setCenter(marker.getPosition());
+    });
 }
 
 // noUI slider
